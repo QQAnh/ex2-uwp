e54973777c932b448a25c87dcf68ec534b33f11c
QQAnh <anhhqd00528@fpt.edu.vn>
2018-09-15 19:25:21 +0700

    new

---
+++ Exc2/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Exc2.View;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Exc2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Type MusicPage = typeof(Music);
+        private Type VideoPage = typeof(Video);
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MusicFrame.Navigate(MusicPage);
+            VideoFrame.Navigate(VideoPage);
+        }
+    }
+}
